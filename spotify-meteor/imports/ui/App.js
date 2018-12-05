2d28c2f7ec9fc0ff3c287941d7fbfa7b4ec89a5a
David Narvaez <dnarvaez27@outlook.com>
2018-12-05 18:31:21 -0500

    Start

--- spotify-meteor/imports/ui/App.js
+++ spotify-meteor/imports/ui/App.js
@@ -5,24 +5,41 @@ import axios from "axios";
 import AccountsUIWrapper from "./AccountsUIWrapper";
 import { withTracker } from "meteor/react-meteor-data";
 import { Meteor } from "meteor/meteor";
+import { Accounts } from 'meteor/accounts-base'
 import Session from "./game_n_lobby/Session/Session";
 import Modal from "./modal/Modal";
 import { CookiesProvider } from "react-cookie";
 
-export const BACKEND_URL = "https://spot-it-fy.herokuapp.com";
-// export const BACKEND_URL = "http://localhost:3000";
+// export const BACKEND_URL = "https://spot-it-fy.herokuapp.com";
+export const BACKEND_URL = "http://localhost:3000";
 
 class App extends Component {
 
   constructor( props ) {
     super( props );
+    
     this.playSongURI = this.playSongURI.bind( this );
     this.toggle = this.toggle.bind( this );
     this.openModal = this.openModal.bind( this );
     this.closeModal = this.closeModal.bind( this );
     this.errorModal = this.errorModal.bind( this );
+    this.openStart = this.openStart.bind( this );
+    
+    this.login = this.login.bind(this);
+    this.openLogin =this.openLogin.bind(this);
+    this.openRegister=this.openRegister.bind(this);
+    this.openProfile = this.openProfile.bind(this);
+    this.openChangePassword = this.openChangePassword.bind(this);
+    this.logout=this.logout.bind(this);
+    this.register = this.register.bind(this);
+    this.validateInput = this.validateInput.bind(this);
+
+    this.usernamesignupinput = React.createRef();
+    this.usernamelogininput = React.createRef();
+
     this.deviceID = undefined;
     this.played = false;
+
     this.modalRef = React.createRef();
     this.imgBannerRef = React.createRef();
   }
@@ -72,6 +89,202 @@ class App extends Component {
     this.modalRef.current.showError( err );
   }
 
+  openStart(){
+    this.openModal( {
+      title: "Start",
+      body: (
+        <div id="start-modal-body-container">
+          <img src="/images/Start.jpg"/>
+          <div id="start-modal-foot-container">
+            <span>
+              Play with your friends and find out who is the Guru of the songs!
+            </span>
+            <div>
+              <button onClick={() => this.openLogin()}>Login</button>
+              <button onClick={() => this.openRegister()}>Signup</button>
+            </div>
+          </div>
+        </div>
+      ),
+      foot: (undefined)
+    } );
+  }
+
+  // User Functions
+  login(username, pass){
+
+    if(!username || !pass){
+      this.showErrorModal("Please provide all the fields");
+    }
+    else{
+      Meteor.loginWithPassword(username, pass, (err)=>{
+        if(err){
+          this.showErrorModal(err.message);
+        }
+        else{
+          this.closeModal();
+        }
+      });
+    }
+  }
+
+  register(username, pass, confirmPass){
+    username = username.replace(/\W/g, '');
+    if(!username || !pass || !confirmPass){
+      this.showErrorModal("Please provide all the fields")
+    }
+    else{
+      if(pass !== confirmPass){
+        this.showErrorModal("Password does not match")
+      }
+      else{
+        Accounts.createUser({
+          username: username,
+          password: pass
+        }, err => {
+          if(err){
+            this.showErrorModal(err.message);
+          }
+          else{
+            this.closeModal();
+          }
+        });
+      }
+    }
+  }
+
+  execOnEnter(e, func, args){
+    if (e.key === 'Enter') {
+      func(...args);
+    }
+  }
+
+  openLogin(){
+    let username, pass;
+    this.openModal( {
+      title: "Login",
+      body: (
+        <div id="login-conatiner">
+          <input 
+            type="text" 
+            placeholder="Username" 
+            onChange={e => username = e.target.value} 
+            onKeyPress={(e) => this.execOnEnter(e, this.login, [username, pass])}
+            onKeyDown={e => this.validateInput(e,this.usernamelogininput)}
+            ref={this.usernamelogininput}/>
+          <input type="password" placeholder="Password" onChange={e => pass = e.target.value} onKeyPress={(e) => this.execOnEnter(e, this.login, [username, pass])}/>
+        </div>
+      ),
+      foot: (
+        <div id="login-footer">
+          <button className="button-cancel" onClick={this.closeModal}>Cancel</button>
+          <button className="button-login" onClick={() => this.login(username, pass)}>Login</button>
+        </div>
+      )
+    } );
+  }
+
+  validateInput(e, inputRef){
+    if (!e.key.match(/[a-zA-Z0-9]/) || e.keyCode === 186) {
+      e.preventDefault();
+    }
+
+    let a = inputRef.current.value.replace(/\W/g, '');
+    if(inputRef.current.value !== a) {
+      inputRef.current.value = a;
+      e.preventDefault();
+    }
+  }
+
+  openRegister(){
+    let username, pass, passCheck;
+    this.openModal( {
+      title: "Signup",
+      body: (
+        <div id="login-conatiner">
+          <input 
+            type="text" 
+            placeholder="Username" 
+            onChange={e => username = e.target.value} 
+            onKeyPress={(e) => this.execOnEnter(e, this.register, [username, pass, passCheck])}
+            onKeyDown={e => this.validateInput(e, this.usernamesignupinput)}
+            ref={this.usernamesignupinput}/>
+          <input type="password" placeholder="Password" onChange={e => pass = e.target.value} onKeyPress={(e) => this.execOnEnter(e, this.register, [username, pass, passCheck])}/>
+          <input type="password" placeholder="Confirm Password" onChange={e => passCheck = e.target.value} onKeyPress={(e) => this.execOnEnter(e, this.register, [username, pass, passCheck])}/>
+        </div>
+      ),
+      foot: (
+        <div id="login-footer">
+          <button className="button-cancel" onClick={this.closeModal}>Cancel</button>
+          <button className="button-login" onClick={() => this.register(username, pass, passCheck)}>Signup</button>
+        </div>
+      )
+    } );
+  }
+
+  openProfile(){
+    this.openModal( {
+      title: this.props.user.username,
+      body: (
+        <div id="login-conatiner">
+
+        </div>
+      ),
+      foot: (
+        <div id="login-footer">
+          <button className="button-cancel" onClick={this.openChangePassword}>Change Password</button>
+          <button className="button-login" onClick={this.logout}>Logout</button>
+        </div>
+      )
+    } );
+  }
+
+  alert(){
+    console.log(123);
+  }
+
+  changePassword(old, newPass, confNewPass){
+    if(newPass !== confNewPass){
+      this.showErrorModal("Password does not match");
+    }
+    else {
+      Accounts.changePassword(old, newPass, (err) => {
+        if(err){
+          this.props.showErrorModal(err);
+        }
+        else {
+          this.openProfile();
+        }
+      });
+    }
+  }
+
+  logout(){
+    Meteor.logout(() => {
+      this.closeModal();
+    });
+  }
+
+  openChangePassword(){
+    let old, newPass, confirmPass;
+    this.openModal( {
+      title: "Change Password",
+      body: (
+        <div id="login-conatiner">
+          <input type="password" placeholder="Current password" onChange={e => old = e.target.value}/>
+          <input type="password" placeholder="New password" onChange={e => newPass = e.target.value}/>
+          <input type="password" placeholder="Confrim new password" onChange={e => confirmPass = e.target.value}/>
+        </div>
+      ),
+      foot: (
+        <div id="login-footer">
+          <button className="button-cancel" onClick={this.openProfile}>Cancel</button>
+          <button className="button-login" onClick={() => this.changePassword(old, newPass, confirmPass)}>Change Password</button>
+        </div>
+      )
+    } );
+  }
+
   render() {
 
     let content = (
@@ -79,7 +292,7 @@ class App extends Component {
         <div>
           <h1>How many songs can you identify?</h1>
           <span>Play it with your friends</span>
-          <button>START</button>
+          <button onClick={this.openStart}>START</button>
         </div>
       </div>
     );
@@ -93,7 +306,10 @@ class App extends Component {
         <AccountsUIWrapper 
           openModal={this.openModal}
           closeModal={this.closeModal}
-          showErrorModal={this.errorModal}/>
+          showErrorModal={this.errorModal}
+          openProfile={this.openProfile}
+          openLogin={this.openLogin}
+          openRegister={this.openRegister}/>
         <div id="spotitfy-img-container">
           <div id="spotitfy-img" ref={this.imgBannerRef}/>
         </div>
